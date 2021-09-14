775cf77659388035152b2c3ff189849030d48d36
David Mihal <david@dmihal.com>
2021-09-14 23:55:57 +0100

    Aztec

---
+++ src/aztec.ts
@@ -0,0 +1,6 @@
+import { Address, BigInt, BigDecimal, ethereum } from "@graphprotocol/graph-ts"
+import { recordTransaction } from "./shared"
+
+export function handleBatch(event: ethereum.Event): void {
+  recordTransaction(event.transaction, 'aztec')
+}
